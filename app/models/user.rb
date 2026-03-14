1753f37a2a3ac881c055efffc1eb8db2ef6694b8
Agne Banzeryte <Agne.banzeryte@gmail.com>
2026-03-14 17:32:33 +0100

    associations set

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_many :chats
+  has_many :messages, through: :chats
+  has_many :favorites
+  has_many :devices, through: :favorites
+  has_many :artists, through: :devices
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 end
