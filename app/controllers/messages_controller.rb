6be43bebd51a7d641f44240cb9c21420328ab1f3
Agne Banzeryte <Agne.banzeryte@gmail.com>
2026-03-24 16:43:01 +0100

    textarea button overlay

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -71,7 +71,7 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message, {}).permit(:content, :role)
+    params.require(:message).permit(:content, :role)
   end
 
   def chat_context
